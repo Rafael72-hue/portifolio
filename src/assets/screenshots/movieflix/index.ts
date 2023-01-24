5012931470fe5f90a54c7c6a2ec9c1575ed9304b
Rafael <rafael_eliu@outlook.com>
2023-01-24 16:03:18 -0300

    1.0.1 - adicionado mais dados para ser exibido na aplicação

---
+++ src/assets/screenshots/movieflix/index.ts
@@ -0,0 +1,6 @@
+import MovieFlixPoster from './poster.png';
+import MovieFlixHomePage from './movieflix-homepage.png';
+import MovieFlixMovieSection from './movieflix-movie_section.png';
+import MovieFlixSearchField from './movieflix-search_field.png';
+
+export { MovieFlixPoster, MovieFlixHomePage, MovieFlixMovieSection, MovieFlixSearchField };
